b160130e9d231cff1796cae4ef89e103cb8e9dcd
hrikbansaha49268 <73658678+hrikbansaha49268@users.noreply.github.com>
2022-07-08 21:17:10 +0530

    Almost done.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Rock, Paper and Scissors</title>
+    </Head>
+    <Component {...pageProps} />
+  </>)
 }
 
 export default MyApp
