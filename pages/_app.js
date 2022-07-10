e9ccf0647752bd620f8491e198551a7ffb593c2c
hrikbansaha49268 <73658678+hrikbansaha49268@users.noreply.github.com>
2022-07-10 12:06:08 +0530

    Ok

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
       <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Stone, Paper and Scissors</title>
+      <title>Rock, Paper and Scissors</title>
     </Head>
     <Component {...pageProps} />
   </>)
