e8ade6584d8afccd912a754c4e2f39b07ef976d0
hrikbansaha49268 <73658678+hrikbansaha49268@users.noreply.github.com>
2022-07-10 18:52:13 +0530

    Updated metadata

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,20 @@ import '../styles/globals.css';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return (<>
-    <Head>
-      <meta charSet="UTF-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Rock, Paper and Scissors</title>
-    </Head>
-    <Component {...pageProps} />
-  </>)
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="description" content="You can play here rock-paper-scisor game. A time pass place for you to play your one of the favoutite childhood game in a digital format." />
+        <meta name="keywords" content="stone game, rock-paper-scisor, rock-paper-scisor game, stone-paper-scissor game, stone paper scissor game, rock paper scissor game" />
+        <meta name="author" content="Innovation" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Rock, Paper and Scissors</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
